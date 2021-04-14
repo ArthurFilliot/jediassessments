1e21bd0ee9f0f187a97dea8999f50d9f6b3b155f
filliot.arthur@gmail.com <filliot.arthur@gmail.com>
2021-04-14 22:54:09 +0200

    Add GalacticStandardCalendar Start N Stop + clean not needed redux-thunk

--- org.jediassessments.ui/src/GalacticStandardCalendarStore.js
+++ org.jediassessments.ui/src/GalacticStandardCalendarStore.js
@@ -1,39 +1,67 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { createAction,handleActions } from 'redux-actions';
-import thunk from 'redux-thunk';
 
 export const galacticStandardCalendarReducer = handleActions(
     {
-        STORE_WINDOW: (state, action) => {
-            state.galacticWindow = action.payload.window;
-        },
         STORE_NOW: (state, action) => {
-            let clientNow = action.payload.clientNow
-            state.nowIndex = state.galacticWindow.dates.findIndex(entry => Math.abs(clientNow - entry.value) < 1000);
-            if (state.nowIndex == -1) {
-                state.nowIndex = state.galacticWindow.dates.length-1;
+            if (action.payload.running) {
+                state.running = action.payload.running;
+            }
+            if (state.running) {
+                let clientNow = action.payload.clientNow
+                let mGalacticWindow = (action.payload.window) ? action.payload.window : state.galacticWindow;
+                state.nowIndex = mGalacticWindow.dates.findIndex(entry => Math.abs(clientNow - entry.value) < 1000);
+                if (state.nowIndex == -1) {
+                    state.nowIndex = state.galacticWindow.dates.length-1;
+                }
+                state.galacticWindow = mGalacticWindow;
             }
             return state;
-        }
+        },
+        START: (state, action) => {
+            state.running = true;
+        },
+        STOP: (state, action) => {
+            state.running = false;
+        } 
     },
     { 
         galacticWindow: {},
         nowIndex: -1, 
+        running: false,
     }
 );
 export const storeWindow = createAction('STORE_WINDOW');
 export const storeNow = createAction('STORE_NOW');
+export const storeStart = createAction('START');
+export const storeStop = createAction('STOP');
 
 const galacticStandardCalendarReducers = combineReducers({galacticStandardCalendar: galacticStandardCalendarReducer});
-const galacticStandardCalendarStore = createStore(galacticStandardCalendarReducers, applyMiddleware(thunk));
+const galacticStandardCalendarStore = createStore(galacticStandardCalendarReducers);
 
-export function updateFull(mGalacticWindow, clientNow) {
-    galacticStandardCalendarStore.dispatch(() => {
-        galacticStandardCalendarStore.dispatch(storeWindow({'window':mGalacticWindow}));
-        galacticStandardCalendarStore.dispatch(storeNow({'clientNow':clientNow}));
-    });
+export function update(clientNow, mGalacticWindow, start) {  
+    let payload = {'clientNow':clientNow}
+    if (mGalacticWindow) {
+        payload.window = mGalacticWindow;
+    }
+    if (start===true) {
+        payload.running = true;
+    }
+    galacticStandardCalendarStore.dispatch(storeNow(payload));
 }
 
-export function updatePartial(clientNow) {
-    galacticStandardCalendarStore.dispatch(storeNow({'clientNow':clientNow}));
+let timer = null;
+export function start(clientNow) {
+    clearInterval(timer);
+    timer = setInterval(()=>{
+        clientNow = clientNow+1000;
+        update(clientNow)
+    }, 1000);
+    galacticStandardCalendarStore.dispatch(storeStart());
+    update(clientNow);
+}
+
+export function stop() {
+    clearInterval(timer);
+    galacticStandardCalendarStore.dispatch(storeStop());
 }
--- org.jediassessments.ui/src/GalacticStandardCalendarStore.test.js
+++ org.jediassessments.ui/src/GalacticStandardCalendarStore.test.js
@@ -1,4 +1,4 @@
-import {galacticStandardCalendarReducer , updateFull, updatePartial} from './GalacticStandardCalendarStore'
+import {galacticStandardCalendarReducer , update} from './GalacticStandardCalendarStore'
 
 function* mockedReponses() {
   yield {dates:[{"key":{"day":1,"period":1,"year":-35},"value":"1618257458000"},{"key":{"day":2,"period":1,"year":-35},"value":"1618257459000"},{"key":{"day":3,"period":1,"year":-35},"value":"1618257460000"},{"key":{"day":4,"period":1,"year":-35},"value":"1618257461000"}]}
@@ -17,49 +17,55 @@ describe('galacticStandardCalendarReducer', () => {
     .toEqual({
       galacticWindow: {},
       nowIndex: -1, 
+      running: false,
     });
 
     let currWindow = responsesGen.next().value;
     let currClientNow = 1618257458955;
 
-    updateFull(currWindow, currClientNow);
+    update(currClientNow, currWindow, true); 
     expect(galacticStandardCalendarReducer(undefined, {}))
     .toEqual({
       galacticWindow: currWindow,
       nowIndex: 0,
+      running: true,
     });
 
     currClientNow = 1618257459001;
-    updatePartial(currClientNow);
+    update(currClientNow);
     expect(galacticStandardCalendarReducer(undefined, {}))
     .toEqual({
       galacticWindow: currWindow,
       nowIndex: 1, 
+      running: true,
     });
 
     currClientNow = 1618257460000;
-    updatePartial(currClientNow);
+    update(currClientNow);
     expect(galacticStandardCalendarReducer(undefined, {}))
     .toEqual({
       galacticWindow: currWindow,
       nowIndex: 2, 
+      running: true,
     });
 
     currClientNow = 1618257464000;
-    updatePartial(currClientNow);
+    update(currClientNow);
     expect(galacticStandardCalendarReducer(undefined, {}))
     .toEqual({
       galacticWindow: currWindow,
       nowIndex: 3, 
+      running: true,
     });
 
     currWindow = responsesGen.next().value;
     currClientNow = 1618257464000;
-    updateFull(currWindow, currClientNow);
+    update(currClientNow, currWindow);
     expect(galacticStandardCalendarReducer(undefined, {}))
     .toEqual({
       galacticWindow: currWindow,
       nowIndex: 1, 
+      running: true,
     });
 
   })
