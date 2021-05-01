b96aba6f5246012d3ed000bb3d254e67d6bdef9d
filliot.arthur@gmail.com <filliot.arthur@gmail.com>
2021-05-01 22:03:39 +0200

    Docker build and run management optimizations

---
+++ org.jediassessments.gateway/start.sh
@@ -0,0 +1,11 @@
+export NOW=$(date '+%Y%m%d%H%M%S')
+echo $NOW
+docker-compose build --force-rm --parallel
+nbbuilders=$(docker images --format "{{.ID}}" --filter label=stage=builder | wc -l)
+echo $nbbuilders
+if [ $nbbuilders -gt 1 ]
+then
+	docker image prune -f --filter label=stage=builder --filter label!=version=$NOW
+fi
+docker image prune -f --filter label=stage=runner
+docker-compose up -d
